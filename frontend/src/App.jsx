cbf6fcfdbe9167229d7227cf3a3eb2c5b6f367d0
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-10-27 17:19:15 +0530

    Fix Navbar import case/extension

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar.jsx'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'

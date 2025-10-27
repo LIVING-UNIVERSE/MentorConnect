3aed93618c0150952d146f6cd5cca3c386d98052
LIVING-UNIVERSE <living.universe.nic@gmail.com>
2025-10-27 18:00:16 +0530

    try to add cors

--- backend/app.js
+++ backend/app.js
@@ -28,32 +28,7 @@
 
 
 
-// import express from 'express'
-// import cors from 'cors'
-// import 'dotenv/config'
-// import adminRouter from './routes/adminRoute.js'
-// import doctorRouter from './routes/doctorRoute.js'
-// import userRouter from './routes/userRoute.js'
-
-// const app = express()
-
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
-// app.use(cors({
-//   origin: ["https://mentor-connect-6jqg.vercel.app", "https://mentor-connect-oaep.vercel.app"],
-//   credentials: true
-// }));
-
-
-// app.use('/api/admin', adminRouter)
-// app.use('/api/doctor', doctorRouter)
-// app.use('/api/user', userRouter)
-
-// app.get('/', (req, res) => res.send('API is working just fine'))
-
-// export default app
-
-
+// C:\DocLook\backend\app.js
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
@@ -65,35 +40,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: ["https://mentor-connect-6jqg.vercel.app", "https://mentor-connect-oaep.vercel.app"],
+  credentials: true
+}));
 
-const whitelist = [
-  "https://mentor-connect-6jqg.vercel.app",
-  "https://mentor-connect-oaep.vercel.app"
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    // allow requests with no origin (like curl, postman, server-to-server)
-    if (!origin) return callback(null, true);
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS: Origin not allowed: " + origin));
-    }
-  },
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // explicitly handle preflight
-
-// debug helper (remove in production)
-app.use((req, res, next) => {
-  console.log('Incoming origin:', req.headers.origin);
-  next();
-});
 
 app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
@@ -102,4 +53,3 @@ app.use('/api/user', userRouter)
 app.get('/', (req, res) => res.send('API is working just fine'))
 
 export default app
-
